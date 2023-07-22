9d4e443342f3a79507f483b81a56de991bb8d234
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2023-07-22 21:34:05 +0200

    write all games to csv file

--- boardmaker.py
+++ boardmaker.py
@@ -1,9 +1,39 @@
 # Goal:
-# Read a list of entries, output 25 of them in random order.
+# Read a list of entries, output 25 of them in random order. Write the output in LaTeX syntax to a file.
+
+# Imports
+import random
+import csv
+
+# Set Constants 
+ENTRIESFILENAME = "entries.txt"
+GAMESFILENAME = "games.csv"
+BOARDSIZE = 9
+GAMELIMIT = 3
+
+# Define Functions
+def list_of_entries(entriesfilename):
+    # Create a list that contains all the possible entries
+    entries = open(entriesfilename).read().split("\n")
+    return entries
+
+def games(entries):
+    # Return a list of all games. Every item contains a sublist with the gameboard.
+    games = ["0",]*GAMELIMIT
+    for i in range(GAMELIMIT):
+        games[i] = random.sample(entries, BOARDSIZE)
+    return games
+
+def writegames(games):
+    # Write each game to a row in a csv file. 
+    with open(GAMESFILENAME, "w") as gamefile:
+        writer = csv.writer(gamefile)
+        for item in games:
+            writer.writerow(item)
 
 
-# Create a tuple that contains all the possible entries
-entriesfilename = "entries.txt"
-entries = open(entriesfilename).read().split("\n")
+entries = list_of_entries(ENTRIESFILENAME)
+games = games(entries)
+writegames(games)
 
-print(entries)
+print(games)
