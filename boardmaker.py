1bb79371945702bc9aba6da36360576be3aac5f7
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2023-07-25 21:43:57 +0200

    tables are now displayed in latex

--- boardmaker.py
+++ boardmaker.py
@@ -1,26 +1,25 @@
 # Goal:
-# Read a list of entries, output 25 of them in random order. Write the output in LaTeX syntax to a file.
+# Read a list of entries, output 25 of them in random order as a LaTeX table.
 
 # Imports
 import random
-import csv
 
-# Set Constants 
+# Constants 
 ENTRIESFILENAME = "entries.txt"
 GAMESFILENAME = "games.csv"
-LATEXFILENAME = "tables.tex"
-BOARDSIZE = 9
+TABLESFILENAME = "tables.tex"
+COLMAX = 5
+BOARDSIZE = pow(COLMAX, 2)
 GAMELIMIT = 3
-COLMAX = 3
 
 # Define Functions
 def list_of_entries(entriesfilename):
-    # Create a list that contains all the possible entries
+    # Return a list with all entries
     entries = open(entriesfilename).read().split("\n")
     return entries
 
 def create_games(entries):
-    # Return a list of all games. Every item contains a sublist with a gameboard.
+    # Return a list of all games. Every item contains a sublist with a unique gameboard.
     games = ["0",]*GAMELIMIT
     for i in range(GAMELIMIT):
         games[i] = random.sample(entries, BOARDSIZE)
@@ -29,20 +28,18 @@ def create_games(entries):
 def games_to_table(games):
     # Write a latex table for every gameboard
     i = 0 # iterate for every game
-    with open(LATEXFILENAME, "w") as latexfile:
+    with open(TABLESFILENAME, "w") as latexfile:
         for sublist in games:
-            latexfile.write("\\begin{tabular}{c|c|c}\hline ")
+            latexfile.write(f"\\begin{{tabularx}}{{\columnwidth}}{{*{{{COLMAX}}}{{|X}}|}}\hline ")
             for item in sublist:
                 i += 1
                 latexfile.write(f"{item} & ")
-                if i % COLMAX == 0:     # Break row after every third item
-                    latexfile.seek(latexfile.tell() - 3)  # Move back two positions to remove the last '& '
+                if i % COLMAX == 0:     # Break the current row after every fifth item
+                    latexfile.seek(latexfile.tell() - 3)    # Move back three positions to remove the last "& "
                     latexfile.write("\\\\\hline ")
-            latexfile.write("\\end{tabular}\\newpage \n")
+            latexfile.write("\\end{tabularx}\\newpage \n")
 
 
 entries = list_of_entries(ENTRIESFILENAME)
 create_games = create_games(entries)
 games_to_table(create_games)
-
-# print(games)
